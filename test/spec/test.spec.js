a3dcb07d691470dad8c80821c205c02392889048
chetani <agarwalatrisha1212@gmail.com>
2018-11-12 06:23:22 +0800

    Inital setup

---
+++ test/spec/test.spec.js
@@ -0,0 +1,10 @@
+/**
+* Provides automated checks of android app: checks of launching of app and navigation through splash screen 
+* @author: Trisha Chetani
+*/ 
+
+describe('Twitter Automation test cases', function () {
+    it('should launch the app and click on continue from splash screen', function () {
+        browser.pause(2000);
+    });
+});
